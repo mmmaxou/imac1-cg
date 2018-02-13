65b6489ab70ace014ed0063b30bcaa8f70dba72f
mmmaxou <mmmaxou@yahoo.fr>
2018-02-13 01:09:35 +0100

    TD2: exercice2: clean and working

--- minimal.c
+++ minimal.c
@@ -9,11 +9,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
+
+#define SEG_NUMBER 50
+#define PI 3.141592
+#define SEG_SPACE PI/SEG_NUMBER
+#define NB_FIG 5
 
 /* Dimensions de la fenêtre */
 static unsigned int WINDOW_WIDTH = 800;
 static unsigned int WINDOW_HEIGHT = 800;
 void resizeWindow();
+void normalized_glVertex2f(int x, int y);
+void drawSquare(int w, int full);
+void drawLandmark(int w);
+void drawCircle(int w, int full);
 
 /* Nombre de bits par pixel de la fenêtre */
 static const unsigned int BIT_PER_PIXEL = 32;
@@ -59,6 +69,7 @@ int main(int argc, char** argv) {
 
   /* Boucle d'affichage */
   int loop = 1;
+  int mode = 3;
 
   while(loop) {
 
@@ -67,6 +78,26 @@ int main(int argc, char** argv) {
     glClear(GL_COLOR_BUFFER_BIT);
 
     /* Placer ici le code de dessin */
+    glColor3ub(0, 0, 255);
+    switch ( mode ) {
+      case 0:
+        drawSquare( 100, 0 );
+        break;
+      case 1:
+        drawLandmark( 100 );
+        break;
+      case 2:
+        drawSquare( 100, 1 );
+        break;
+      case 3:
+        drawCircle( 100, 0 );
+        break;
+      case 4:
+        drawCircle( 100, 1 );
+        break;
+      default:
+        break;
+    }
 
     /* Echange du front et du back buffer : mise à jour de la fenêtre */
     SDL_GL_SwapBuffers();
@@ -87,6 +118,7 @@ int main(int argc, char** argv) {
 
           /* Clic souris */
         case SDL_MOUSEBUTTONUP:
+          mode = (mode+1) % NB_FIG;
           break;
 
         case SDL_MOUSEBUTTONDOWN:
@@ -94,6 +126,10 @@ int main(int argc, char** argv) {
 
           /* Touche clavier */
         case SDL_KEYDOWN:
+          if (e.key.keysym.sym == SDLK_q) {
+            loop = 0;
+            printf("Quit\n");
+          }
           break;
 
         case SDL_KEYUP:
@@ -135,5 +171,58 @@ void resizeWindow() {
   SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE);
 
   glClear(GL_COLOR_BUFFER_BIT);
+
+}
+void normalized_glVertex2f(int x, int y) {
+  glVertex2f(-1+2.*x/WINDOW_WIDTH,-(-1+2.*y/WINDOW_HEIGHT)); 
+}
+void drawSquare(int w, int full) {
+  int x = (WINDOW_WIDTH/2) - w/2;
+  int y = (WINDOW_HEIGHT/2) - w/2;
+  if ( full == 1 )
+    glBegin(GL_QUADS);
+  else
+    glBegin(GL_LINE_LOOP);
+  normalized_glVertex2f(x, y);
+  normalized_glVertex2f(x, y+w);
+  normalized_glVertex2f(x+w, y+w);
+  normalized_glVertex2f(x+w, y);
+  glEnd();
+}
+void drawLandmark(int w) {  
+  int x = (WINDOW_WIDTH/2);
+  int y = (WINDOW_HEIGHT/2);
+  glColor3ub(255, 0, 0);
+  glBegin(GL_LINES);
+  normalized_glVertex2f(x, y);
+  normalized_glVertex2f(x, y-w);
+  glEnd();
+  glColor3ub(0, 255, 0);
+  glBegin(GL_LINES);
+  normalized_glVertex2f(x, y);
+  normalized_glVertex2f(x+w, y);
+  glEnd();
+
+}
+void drawCircle(int w, int full) {
+  int i;
+  float x, y;
+  int center_x = (WINDOW_WIDTH/2);
+  int center_y = (WINDOW_HEIGHT/2);
+  
+  if ( full == 1 )
+    glBegin(GL_POLYGON);
+  else
+    glBegin(GL_LINE_LOOP);
+  for ( i=0; i<SEG_NUMBER; i++) {
+    x = cos(SEG_SPACE * i * 2) * w;
+    y = sin(SEG_SPACE * i * 2) * w;
+    /*
+    printf("SegSpace:%f", SEG_SPACE);
+    printf("x:%f _ y:%f\n",x,y);
+    */
+    normalized_glVertex2f(center_x + x , center_y + y);
+  }
+  glEnd();
 
 }
