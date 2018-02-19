e611d2fe5d9e3a401620fd7f5ec043c862d07df5
PLUCHARD MAXIMILIEN <mpluchar@localhost.localdomain>
2018-02-19 12:37:15 +0100

    drawRoundedSquare

--- TD3/minimal.c
+++ TD3/minimal.c
@@ -10,38 +10,31 @@
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
+#include <time.h>
 
 #define SEG_NUMBER 50
 #define PI 3.141592
 #define SEG_SPACE PI/SEG_NUMBER
 #define NB_FIG 5
+#define SCALE_X 8
+#define SCALE_Y 6
+
 
 /* Dimensions de la fenêtre */
-static unsigned int WINDOW_WIDTH = 800;
-static unsigned int WINDOW_HEIGHT = 800;
+static unsigned int WINDOW_WIDTH = 100 * SCALE_X;
+static unsigned int WINDOW_HEIGHT = 100 * SCALE_Y;
 void resizeWindow();
 void normalized_glVertex2f(int x, int y);
-void drawSquare(int w, int full);
-void drawLandmark(int w);
-void drawCircle(int w, int full);
+void drawSquare(int full);
+void drawLandmark();
+void drawCircle(int full);
+void drawRoundedSquare(int full);
 
 /* Nombre de bits par pixel de la fenêtre */
 static const unsigned int BIT_PER_PIXEL = 32;
 
 /* Nombre minimal de millisecondes separant le rendu de deux images */
 static const Uint32 FRAMERATE_MILLISECONDS = 1000 / 60;
-/*
-static const unsigned char COLORS[] = {
-  0, 0, 0 ,
-  255, 0, 0 ,
-  0, 255, 0 ,
-  0, 0, 255 ,
-  255, 255, 0 ,
-  255, 0, 255 ,
-  0, 255, 255 ,
-  255, 255, 255 
-};
-*/
 
 int main(int argc, char** argv) {
 
@@ -61,15 +54,18 @@ int main(int argc, char** argv) {
 
   /* Titre de la fenêtre */
   SDL_WM_SetCaption("J'aime les pates au beurre", NULL);
+  resizeWindow();
   glClearColor(0.1, 0.5, 0.8, 1);
   glClear(GL_COLOR_BUFFER_BIT);
+  glMatrixMode(GL_MODELVIEW);
 
   /* On veut dessiner en tres tres bleu */
   glColor3ub(0, 0, 255);
 
   /* Boucle d'affichage */
   int loop = 1;
-  int mode = 3;
+  
+  srand(time(NULL));
 
   while(loop) {
 
@@ -78,26 +74,10 @@ int main(int argc, char** argv) {
     glClear(GL_COLOR_BUFFER_BIT);
 
     /* Placer ici le code de dessin */
-    glColor3ub(0, 0, 255);
-    switch ( mode ) {
-      case 0:
-        drawSquare( 100, 0 );
-        break;
-      case 1:
-        drawLandmark( 100 );
-        break;
-      case 2:
-        drawSquare( 100, 1 );
-        break;
-      case 3:
-        drawCircle( 100, 0 );
-        break;
-      case 4:
-        drawCircle( 100, 1 );
-        break;
-      default:
-        break;
-    }
+    glColor3ub(255, 0, 0);
+    drawRoundedSquare( 1 );
+
+    
 
     /* Echange du front et du back buffer : mise à jour de la fenêtre */
     SDL_GL_SwapBuffers();
@@ -118,10 +98,13 @@ int main(int argc, char** argv) {
 
           /* Clic souris */
         case SDL_MOUSEBUTTONUP:
-          mode = (mode+1) % NB_FIG;
           break;
 
         case SDL_MOUSEBUTTONDOWN:
+          
+          break;
+          
+        case SDL_MOUSEMOTION:
           break;
 
           /* Touche clavier */
@@ -166,7 +149,7 @@ void resizeWindow() {
   glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluOrtho2D(-1., 1., -1., 1.);
+  gluOrtho2D((float) -SCALE_X/2, (float) SCALE_X/2, (float) -SCALE_Y/2, (float) SCALE_Y/2);
 
   SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE);
 
@@ -176,53 +159,65 @@ void resizeWindow() {
 void normalized_glVertex2f(int x, int y) {
   glVertex2f(-1+2.*x/WINDOW_WIDTH,-(-1+2.*y/WINDOW_HEIGHT)); 
 }
-void drawSquare(int w, int full) {
-  int x = (WINDOW_WIDTH/2) - w/2;
-  int y = (WINDOW_HEIGHT/2) - w/2;
+void drawSquare(int full) {
   if ( full == 1 )
     glBegin(GL_QUADS);
   else
     glBegin(GL_LINE_LOOP);
-  normalized_glVertex2f(x, y);
-  normalized_glVertex2f(x, y+w);
-  normalized_glVertex2f(x+w, y+w);
-  normalized_glVertex2f(x+w, y);
+  glVertex2f(-0.5, -0.5);
+  glVertex2f(-0.5, 0.5);
+  glVertex2f(0.5, 0.5);
+  glVertex2f(0.5, -0.5);
   glEnd();
 }
-void drawLandmark(int w) {  
-  int x = (WINDOW_WIDTH/2);
-  int y = (WINDOW_HEIGHT/2);
+void drawLandmark() {  
   glColor3ub(255, 0, 0);
   glBegin(GL_LINES);
-  normalized_glVertex2f(x, y);
-  normalized_glVertex2f(x, y-w);
-  glEnd();
+  glVertex2f(0, 0);
+  glVertex2f(1, 0);
+  
   glColor3ub(0, 255, 0);
-  glBegin(GL_LINES);
-  normalized_glVertex2f(x, y);
-  normalized_glVertex2f(x+w, y);
+  glVertex2f(0, 0);
+  glVertex2f(0, 1);
   glEnd();
 
 }
-void drawCircle(int w, int full) {
+void drawCircle(int full) {
   int i;
-  float x, y;
-  int center_x = (WINDOW_WIDTH/2);
-  int center_y = (WINDOW_HEIGHT/2);
-  
+  float x, y;  
+
   if ( full == 1 )
     glBegin(GL_POLYGON);
   else
     glBegin(GL_LINE_LOOP);
   for ( i=0; i<SEG_NUMBER; i++) {
-    x = cos(SEG_SPACE * i * 2) * w;
-    y = sin(SEG_SPACE * i * 2) * w;
-    /*
-    printf("SegSpace:%f", SEG_SPACE);
-    printf("x:%f _ y:%f\n",x,y);
-    */
-    normalized_glVertex2f(center_x + x , center_y + y);
+    x = cos(SEG_SPACE * i * 2)/2;
+    y = sin(SEG_SPACE * i * 2)/2;
+    glVertex2f(x , y);
   }
   glEnd();
-
+}
+void drawRoundedSquare(int full) {  
+  glPushMatrix();
+    glScalef(0.5, 1, 1);
+    drawSquare(1);
+    glScalef(2, 0.5, 1);
+    drawSquare(1);
+  glPopMatrix();
+  
+  glPushMatrix();
+    glScalef(0.5, 0.5, 1);
+    
+    glTranslatef(-0.5, -0.5, 0);    
+    drawCircle(1);
+  
+    glTranslatef(1, 0, 0);
+    drawCircle(1);
+    
+    glTranslatef(0, 1, 0);    
+    drawCircle(1);
+  
+    glTranslatef(-1, 0, 0);
+    drawCircle(1);
+  glPopMatrix();  
 }
