8e801db3e034bc0cb0eb498cd8e979b9dfdfe430
PLUCHARD MAXIMILIEN <mpluchar@localhost.localdomain>
2018-02-19 13:07:50 +0100

    drawFirstArm

--- TD3/minimal.c
+++ TD3/minimal.c
@@ -16,19 +16,19 @@
 #define PI 3.141592
 #define SEG_SPACE PI/SEG_NUMBER
 #define NB_FIG 5
-#define SCALE_X 8
-#define SCALE_Y 6
 
 
 /* Dimensions de la fenêtre */
-static unsigned int WINDOW_WIDTH = 100 * SCALE_X;
-static unsigned int WINDOW_HEIGHT = 100 * SCALE_Y;
+static unsigned int WINDOW_WIDTH = 800;
+static unsigned int WINDOW_HEIGHT = 600;
 void resizeWindow();
 void normalized_glVertex2f(int x, int y);
 void drawSquare(int full);
 void drawLandmark();
 void drawCircle(int full);
-void drawRoundedSquare(int full);
+void drawRoundedSquare(float sphereRadius, int full);
+void drawFirstArm(int full);
+void drawSecondArm(int full);
 
 /* Nombre de bits par pixel de la fenêtre */
 static const unsigned int BIT_PER_PIXEL = 32;
@@ -57,7 +57,6 @@ int main(int argc, char** argv) {
   resizeWindow();
   glClearColor(0.1, 0.5, 0.8, 1);
   glClear(GL_COLOR_BUFFER_BIT);
-  glMatrixMode(GL_MODELVIEW);
 
   /* On veut dessiner en tres tres bleu */
   glColor3ub(0, 0, 255);
@@ -74,10 +73,24 @@ int main(int argc, char** argv) {
     glClear(GL_COLOR_BUFFER_BIT);
 
     /* Placer ici le code de dessin */
-    glColor3ub(255, 0, 0);
-    drawRoundedSquare( 1 );
-
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
     
+    glColor3ub(255, 0, 0);
+    
+    glPushMatrix();
+      glTranslatef(-3, 2, 0);
+      drawRoundedSquare( 0.2, 0 );
+    glPopMatrix();
+    
+    glPushMatrix();
+      glTranslatef(-1, 2, 0);
+      glScalef(0.5, 0.5, 1);
+      drawFirstArm( 0 );
+    glPopMatrix();
+    
+    
+       
 
     /* Echange du front et du back buffer : mise à jour de la fenêtre */
     SDL_GL_SwapBuffers();
@@ -149,7 +162,7 @@ void resizeWindow() {
   glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluOrtho2D((float) -SCALE_X/2, (float) SCALE_X/2, (float) -SCALE_Y/2, (float) SCALE_Y/2);
+  gluOrtho2D(-4, 4, -3, 3);
 
   SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE);
 
@@ -197,27 +210,67 @@ void drawCircle(int full) {
   }
   glEnd();
 }
-void drawRoundedSquare(int full) {  
+void drawRoundedSquare(float sphereRadius, int full) {  
+  
   glPushMatrix();
-    glScalef(0.5, 1, 1);
-    drawSquare(1);
-    glScalef(2, 0.5, 1);
-    drawSquare(1);
+    glScalef(1-2*sphereRadius, 1, 1);
+    drawSquare(full);
+  glPopMatrix();
+  glPushMatrix();
+    glScalef(1, 1-2*sphereRadius, 1);
+    drawSquare(full);
   glPopMatrix();
   
   glPushMatrix();
-    glScalef(0.5, 0.5, 1);
     
-    glTranslatef(-0.5, -0.5, 0);    
-    drawCircle(1);
-  
-    glTranslatef(1, 0, 0);
-    drawCircle(1);
+    glPushMatrix();
+      glTranslatef(-0.5+sphereRadius, -0.5+sphereRadius, 0);    
+      glScalef(sphereRadius*2, sphereRadius*2, 1);
+      drawCircle(full);
+    glPopMatrix();
     
-    glTranslatef(0, 1, 0);    
-    drawCircle(1);
+    glPushMatrix();
+      glTranslatef(-0.5+sphereRadius, 0.5-sphereRadius, 0);    
+      glScalef(sphereRadius*2, sphereRadius*2, 1);
+      drawCircle(full);
+    glPopMatrix();
+    
+    glPushMatrix();
+      glTranslatef(0.5-sphereRadius, -0.5+sphereRadius, 0);    
+      glScalef(sphereRadius*2, sphereRadius*2, 1);
+      drawCircle(full);
+    glPopMatrix();
+    
+    glPushMatrix();
+      glTranslatef(0.5-sphereRadius, 0.5-sphereRadius, 0);    
+      glScalef(sphereRadius*2, sphereRadius*2, 1);
+      drawCircle(full);
+    glPopMatrix();
   
-    glTranslatef(-1, 0, 0);
-    drawCircle(1);
   glPopMatrix();  
+}
+void drawFirstArm(int full) {
+  glPushMatrix();
+    glScalef(2, 2, 1);
+    drawCircle( full );
+  glPopMatrix();
+  
+  glPushMatrix();
+    glTranslatef(3, 0, 0);
+    drawCircle( full );
+  glPopMatrix();
+  
+  if ( full )
+    glBegin(GL_QUADS);
+  else 
+    glBegin(GL_LINE_LOOP);
+  glVertex2f(0, 1);
+  glVertex2f(3, 0.5);
+  glVertex2f(3, -0.5);
+  glVertex2f(0, -1);
+  glEnd();
+  
+}
+void drawSecondArm(int full) {
+  
 }
