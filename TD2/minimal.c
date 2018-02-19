bd55f81d993b319669c93f4a237781ebdedc139f
PLUCHARD MAXIMILIEN <mpluchar@localhost.localdomain>
2018-02-19 12:15:04 +0100

    TD 2 : DONE

--- TD2/minimal.c
+++ TD2/minimal.c
@@ -10,15 +10,19 @@
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
+#include <time.h>
 
 #define SEG_NUMBER 50
 #define PI 3.141592
 #define SEG_SPACE PI/SEG_NUMBER
 #define NB_FIG 5
+#define SCALE_X 8
+#define SCALE_Y 6
+
 
 /* Dimensions de la fenêtre */
-static unsigned int WINDOW_WIDTH = 800;
-static unsigned int WINDOW_HEIGHT = 600;
+static unsigned int WINDOW_WIDTH = 100 * SCALE_X;
+static unsigned int WINDOW_HEIGHT = 100 * SCALE_Y;
 void resizeWindow();
 void normalized_glVertex2f(int x, int y);
 void drawSquare(int full);
@@ -52,49 +56,99 @@ int main(int argc, char** argv) {
   resizeWindow();
   glClearColor(0.1, 0.5, 0.8, 1);
   glClear(GL_COLOR_BUFFER_BIT);
+  glMatrixMode(GL_MODELVIEW);
 
   /* On veut dessiner en tres tres bleu */
   glColor3ub(0, 0, 255);
 
   /* Boucle d'affichage */
   int loop = 1;
-  int mode = 3;
+  float center_x = 0;
+  float center_y = 0;
+  int rotating = 0;
+  int rotate_start_x = 0;
+  int rotate_start_y = 0;
+  int rotate_angle = 0;
+  float rand_x = 0;
+  float rand_y = 0;
+  
+  srand(time(NULL));
 
   while(loop) {
 
     /* Récupération du temps au début de la boucle */
     Uint32 startTime = SDL_GetTicks();
     glClear(GL_COLOR_BUFFER_BIT);
-    
-    /* Changement de la matrice */
 
     /* Placer ici le code de dessin */
-    glColor3ub(0, 0, 255);
+    drawLandmark();
+    float delta_x = (float) rand() /(float) RAND_MAX;
+    float delta_y = (float) rand() /(float) RAND_MAX;
+    delta_x -= 0.5;
+    delta_y -= 0.5;
+    rand_x += delta_x / 5;
+    rand_y += delta_y / 5;
+    
+    if ( rand_x >= SCALE_X/2 ) rand_x = SCALE_X/2;
+    if ( rand_y >= SCALE_Y/2 ) rand_y = SCALE_Y/2;
+    if ( rand_x <= -SCALE_X/2 ) rand_x = -SCALE_X/2;
+    if ( rand_y <= -SCALE_Y/2 ) rand_y = -SCALE_Y/2;
+    
+    /* 
+      Changement de la matrice
+      Cercle centré en (1, 2)
+    */
+    glColor3ub(255, 120, 0);
+    glTranslatef(1, 2, 0);
+    drawCircle( 1 );
+    glTranslatef(-1, -2, 0);
     
     /*
-    switch ( mode ) {
-      case 0:
-        drawSquare( 100, 0 );
-        break;
-      case 1:
-        drawLandmark( );
-        break;
-      case 2:
-        drawSquare( 100, 1 );
-        break;
-      case 3:
-        drawCircle( 100, 0 );
-        break;
-      case 4:
-        drawCircle( 100, 1 );
-        break;
-      default:
-        break;
-    }
+      Carré
+      Rotation de 45 degres
+      Translation de 2 axe des X
     */
-    drawLandmark();
-    drawSquare( 0 );
-    drawCircle( 0 );
+    int alpha = 45;
+    glColor3ub(255, 0, 0);
+    glRotatef(alpha, 0.0, 0.0, 1.0);
+    glTranslatef(2, 0, 0);
+    drawSquare( 1 );    
+    glTranslatef(-2, 0, 0);
+    glRotatef(-alpha, 0.0, 0.0, 1.0);   
+    
+    /*
+      Carré
+      Rotation de 45 degres
+      Translation de 2 axe des X
+    */
+    glColor3ub(255, 0, 255);
+    glTranslatef(2, 0, 0);
+    glRotatef(alpha, 0.0, 0.0, 1.0);
+    drawSquare( 1 );    
+    glRotatef(-alpha, 0.0, 0.0, 1.0);    
+    glTranslatef(-2, 0, 0);
+    
+    /*
+      Carré jaune centré en la pos de la souris
+    */
+    glColor3ub(255, 255, 0);
+    glTranslatef(center_x, center_y, 0);
+    glRotatef(rotate_angle, 0.0, 0.0, 1.0);
+    drawSquare( 1 );  
+    glRotatef(-rotate_angle, 0.0, 0.0, 1.0); 
+    glTranslatef(-center_x, -center_y, 0);
+    
+    /*
+      Cercle bleu aleatoire
+    */
+    glColor3ub(0, 0, 255);
+    glTranslatef(rand_x, rand_y, 0);
+    drawCircle( 1 );
+    glTranslatef(-rand_x, -rand_y, 0);
+    
+    
+
+    
 
     /* Echange du front et du back buffer : mise à jour de la fenêtre */
     SDL_GL_SwapBuffers();
@@ -115,11 +169,27 @@ int main(int argc, char** argv) {
 
           /* Clic souris */
         case SDL_MOUSEBUTTONUP:
-          mode = (mode+1) % NB_FIG;
+          if ( e.button.button == SDL_BUTTON_RIGHT ) {
+            rotating = 0;
+          }
           break;
 
         case SDL_MOUSEBUTTONDOWN:
-          printf("clic en (%d, %d)\n", e.button.x, e.button.y);
+          if ( e.button.button == SDL_BUTTON_LEFT ) {
+            /* printf("clic en (%d, %d)\n", e.button.x, e.button.y); */
+            center_x = -(SCALE_X/2) + (float) SCALE_X * e.button.x/WINDOW_WIDTH;
+            center_y = -(-(SCALE_Y/2) + (float) SCALE_Y * e.button.y/WINDOW_HEIGHT);
+          } else {
+            rotating = 1;
+            rotate_start_x = e.button.x;
+            rotate_start_y = e.button.y;
+          }
+          break;
+          
+        case SDL_MOUSEMOTION:
+          if ( rotating == 1 ) {
+            rotate_angle = ( rotate_start_x - e.button.x ) + ( rotate_start_y - e.button.y );
+          }
           break;
 
           /* Touche clavier */
@@ -164,7 +234,7 @@ void resizeWindow() {
   glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluOrtho2D(-4., 4., -3., 3.);
+  gluOrtho2D((float) -SCALE_X/2, (float) SCALE_X/2, (float) -SCALE_Y/2, (float) SCALE_Y/2);
 
   SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE);
 
