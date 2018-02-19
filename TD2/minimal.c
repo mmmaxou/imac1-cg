c15fbe02728b9c3f0e0a044ff8896008ecf55f80
PLUCHARD MAXIMILIEN <mpluchar@localhost.localdomain>
2018-02-19 11:19:02 +0100

    objets canoniques

--- TD2/minimal.c
+++ TD2/minimal.c
@@ -18,30 +18,18 @@
 
 /* Dimensions de la fenêtre */
 static unsigned int WINDOW_WIDTH = 800;
-static unsigned int WINDOW_HEIGHT = 800;
+static unsigned int WINDOW_HEIGHT = 600;
 void resizeWindow();
 void normalized_glVertex2f(int x, int y);
-void drawSquare(int w, int full);
-void drawLandmark(int w);
-void drawCircle(int w, int full);
+void drawSquare(int full);
+void drawLandmark();
+void drawCircle(int full);
 
 /* Nombre de bits par pixel de la fenêtre */
 static const unsigned int BIT_PER_PIXEL = 32;
 
 /* Nombre minimal de millisecondes separant le rendu de deux images */
 static const Uint32 FRAMERATE_MILLISECONDS = 1000 / 60;
-/*
-static const unsigned char COLORS[] = {
-  0, 0, 0 ,
-  255, 0, 0 ,
-  0, 255, 0 ,
-  0, 0, 255 ,
-  255, 255, 0 ,
-  255, 0, 255 ,
-  0, 255, 255 ,
-  255, 255, 255 
-};
-*/
 
 int main(int argc, char** argv) {
 
@@ -61,6 +49,7 @@ int main(int argc, char** argv) {
 
   /* Titre de la fenêtre */
   SDL_WM_SetCaption("J'aime les pates au beurre", NULL);
+  resizeWindow();
   glClearColor(0.1, 0.5, 0.8, 1);
   glClear(GL_COLOR_BUFFER_BIT);
 
@@ -76,15 +65,19 @@ int main(int argc, char** argv) {
     /* Récupération du temps au début de la boucle */
     Uint32 startTime = SDL_GetTicks();
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    /* Changement de la matrice */
 
     /* Placer ici le code de dessin */
     glColor3ub(0, 0, 255);
+    
+    /*
     switch ( mode ) {
       case 0:
         drawSquare( 100, 0 );
         break;
       case 1:
-        drawLandmark( 100 );
+        drawLandmark( );
         break;
       case 2:
         drawSquare( 100, 1 );
@@ -98,6 +91,10 @@ int main(int argc, char** argv) {
       default:
         break;
     }
+    */
+    drawLandmark();
+    drawSquare( 0 );
+    drawCircle( 0 );
 
     /* Echange du front et du back buffer : mise à jour de la fenêtre */
     SDL_GL_SwapBuffers();
@@ -122,6 +119,7 @@ int main(int argc, char** argv) {
           break;
 
         case SDL_MOUSEBUTTONDOWN:
+          printf("clic en (%d, %d)\n", e.button.x, e.button.y);
           break;
 
           /* Touche clavier */
@@ -166,7 +164,7 @@ void resizeWindow() {
   glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluOrtho2D(-1., 1., -1., 1.);
+  gluOrtho2D(-4., 4., -3., 3.);
 
   SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE);
 
@@ -176,52 +174,41 @@ void resizeWindow() {
 void normalized_glVertex2f(int x, int y) {
   glVertex2f(-1+2.*x/WINDOW_WIDTH,-(-1+2.*y/WINDOW_HEIGHT)); 
 }
-void drawSquare(int w, int full) {
-  int x = (WINDOW_WIDTH/2) - w/2;
-  int y = (WINDOW_HEIGHT/2) - w/2;
+void drawSquare(int full) {
   if ( full == 1 )
     glBegin(GL_QUADS);
   else
     glBegin(GL_LINE_LOOP);
-  normalized_glVertex2f(x, y);
-  normalized_glVertex2f(x, y+w);
-  normalized_glVertex2f(x+w, y+w);
-  normalized_glVertex2f(x+w, y);
+  glVertex2f(-0.5, -0.5);
+  glVertex2f(-0.5, 0.5);
+  glVertex2f(0.5, 0.5);
+  glVertex2f(0.5, -0.5);
   glEnd();
 }
-void drawLandmark(int w) {  
-  int x = (WINDOW_WIDTH/2);
-  int y = (WINDOW_HEIGHT/2);
+void drawLandmark() {  
   glColor3ub(255, 0, 0);
   glBegin(GL_LINES);
-  normalized_glVertex2f(x, y);
-  normalized_glVertex2f(x, y-w);
-  glEnd();
+  glVertex2f(0, 0);
+  glVertex2f(1, 0);
+  
   glColor3ub(0, 255, 0);
-  glBegin(GL_LINES);
-  normalized_glVertex2f(x, y);
-  normalized_glVertex2f(x+w, y);
+  glVertex2f(0, 0);
+  glVertex2f(0, 1);
   glEnd();
 
 }
-void drawCircle(int w, int full) {
+void drawCircle(int full) {
   int i;
-  float x, y;
-  int center_x = (WINDOW_WIDTH/2);
-  int center_y = (WINDOW_HEIGHT/2);
-  
+  float x, y;  
+
   if ( full == 1 )
     glBegin(GL_POLYGON);
   else
     glBegin(GL_LINE_LOOP);
   for ( i=0; i<SEG_NUMBER; i++) {
-    x = cos(SEG_SPACE * i * 2) * w;
-    y = sin(SEG_SPACE * i * 2) * w;
-    /*
-    printf("SegSpace:%f", SEG_SPACE);
-    printf("x:%f _ y:%f\n",x,y);
-    */
-    normalized_glVertex2f(center_x + x , center_y + y);
+    x = cos(SEG_SPACE * i * 2)/2;
+    y = sin(SEG_SPACE * i * 2)/2;
+    glVertex2f(x , y);
   }
   glEnd();
 
