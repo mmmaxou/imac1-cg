8622210b09e9ac05654e4059f4dad9776bb80574
PLUCHARD MAXIMILIEN <mpluchar@localhost.localdomain>
2018-03-12 12:53:18 +0100

    planet zoom

--- TD5-EX2/minimal.c
+++ TD5-EX2/minimal.c
@@ -10,11 +10,11 @@
 
 #define SEG_NUMBER 50
 #define SEG_SPACE PI/SEG_NUMBER
-#define PI 3.141592
+#define PI 3.14159265
+#define RAD PI / 180.0
 
 #define ORBITAL_BODIES 20
-#define ORBITAL_SPEED_DIVIDER 30
-#define WINDOW_SCALE 60
+#define ORBITAL_SPEED_DIVIDER 100
 #define ORBITAL_BODIES_SCALE 20
 
 typedef struct planet {
@@ -28,8 +28,10 @@ typedef struct planet {
 
 static unsigned int WINDOW_WIDTH = 800;
 static unsigned int WINDOW_HEIGHT = 800;
+static float WINDOW_SCALE = 5;
 static const unsigned int BIT_PER_PIXEL = 32;
 static const Uint32 FRAMERATE_MILLISECONDS = 1000 / 60;
+static int planetCentered = 0;
 
 void drawLandmark();
 void drawSquare(int full);
@@ -37,9 +39,11 @@ void drawCircle(int full);
 void resizeViewport();
 Planet createPlanet(char name[100], float size, float sun_dist, float rot_speed, int r, int g, int b);
 void drawPlanet(Planet p);
+void centrerPlanet(Planet p);
 
 int main(int argc, char** argv) {
   int i;
+  
   // Initialisation de la SDL
   if(-1 == SDL_Init(SDL_INIT_VIDEO)) {
       fprintf(stderr, "Impossible d'initialiser la SDL. Fin du programme.\n");
@@ -68,7 +72,7 @@ int main(int argc, char** argv) {
   systemeSolaire[5] = createPlanet("jupiter", 0.000466667, 5.458, 13, 255, 204, 102);
   systemeSolaire[6] = createPlanet("saturn", 0.0004, 10.123, 9, 255, 51, 204);
   systemeSolaire[7] = createPlanet("uranus", 0.000166, 20.11, 6.8, 102, 204, 255);
-  systemeSolaire[8] = createPlanet("neptune", 0.00016, 30.33, 5.43, 102, 204, 255);
+  systemeSolaire[8] = createPlanet("neptune", 0.00016, 30.33, 5.43, 10, 255, 10);
 
   int loop = 1;
   glClearColor(0.1, 0.1, 0.1 ,1.0); 
@@ -79,6 +83,7 @@ int main(int argc, char** argv) {
 
     // TODO: Code de dessin
     glClear(GL_COLOR_BUFFER_BIT);
+    centrerPlanet(systemeSolaire[planetCentered]);
     
     for (i=0; i<ORBITAL_BODIES; i++) {
       if ( systemeSolaire[i] ) drawPlanet(systemeSolaire[i]);
@@ -90,30 +95,50 @@ int main(int argc, char** argv) {
     while(SDL_PollEvent(&e)) {
 
       switch(e.type) {
-
         case SDL_QUIT:
             loop = 0;
             break;
 
         /* Touche clavier */
         case SDL_KEYDOWN:
+          /* LE ZERO est 256 */
+//           printf("touche pressée (code = %d, char = '%c')\n", e.key.keysym.sym, e.key.keysym.sym);
           if (e.key.keysym.sym == SDLK_q) {
             loop = 0;
             printf("Quit\n");
           }
+          if (e.key.keysym.sym >= 256 && e.key.keysym.sym <= 264) {
+            planetCentered = e.key.keysym.sym-256;
+            printf("Focus on %s\n", systemeSolaire[planetCentered]->name);
+          }
           break;
-          
-        case SDL_MOUSEWHEEL:
-          printf("Wheel: %d", e.wheel.y);
 
         case SDL_VIDEORESIZE:
             WINDOW_WIDTH = e.resize.w;
             WINDOW_HEIGHT = e.resize.h;
             resizeViewport();
+          break;
 
+        case SDL_MOUSEBUTTONUP:
+          switch(e.button.button) {
+            case SDL_BUTTON_WHEELUP: 
+              WINDOW_SCALE += WINDOW_SCALE / 5;
+              printf("Scale:%f\n", WINDOW_SCALE);
+              break;
+            case SDL_BUTTON_WHEELDOWN: 
+              WINDOW_SCALE -= WINDOW_SCALE / 5;
+              WINDOW_SCALE = WINDOW_SCALE < 0.00001 ? 0.00001 : WINDOW_SCALE;
+              printf("Scale:%f\n", WINDOW_SCALE);
+              break;
+          }
+          resizeViewport();
+          break;
+          
         default:
             break;
       }
+      
+      
     }
 
     SDL_GL_SwapBuffers();
@@ -145,14 +170,33 @@ Planet createPlanet(char name[100], float size, float sun_dist, float rot_speed,
   tmp->b = b;
   return tmp;
 }
+void centrerPlanet(Planet p) {
+  float planet_x, planet_y;
+  planet_x = cos(p->angle * 2 * PI /360.0) * p->sun_dist;
+  planet_y = sin(p->angle * 2 * PI /360.0) * p->sun_dist;
+  
+//  glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(
+    -WINDOW_SCALE/2 + planet_x,
+    WINDOW_SCALE/2 + planet_x,
+    -WINDOW_SCALE/2 + planet_y,
+    WINDOW_SCALE/2 + planet_y);
+//  SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_RESIZABLE);
+  
+}
 void drawPlanet(Planet p) {
   if (!p) return;
   glPushMatrix();
     glColor3ub(p->r, p->g, p->b);
     glRotatef(p->angle, 0, 0, 1);
-    glTranslatef(0, p->sun_dist, 0);
-    glScalef(p->size, p->size, 1);      
-    glScalef(WINDOW_SCALE * ORBITAL_BODIES_SCALE, WINDOW_SCALE * ORBITAL_BODIES_SCALE, 1);      
+    glTranslatef(p->sun_dist, 0, 0);
+  
+    // Fix for small windows
+//    glScalef(ORBITAL_BODIES_SCALE * WINDOW_SCALE, ORBITAL_BODIES_SCALE * WINDOW_SCALE, 1);    
+    glScalef(ORBITAL_BODIES_SCALE, ORBITAL_BODIES_SCALE, 1);    
+    glScalef(p->size, p->size, 1);
     drawCircle(1);
   glPopMatrix();
   
@@ -202,6 +246,10 @@ void resizeViewport() {
   glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluOrtho2D(-WINDOW_SCALE/2, WINDOW_SCALE/2, -WINDOW_SCALE/2, WINDOW_SCALE/2);
+  gluOrtho2D(
+    -WINDOW_SCALE/2,
+    WINDOW_SCALE/2,
+    -WINDOW_SCALE/2,
+    WINDOW_SCALE/2);
   SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_RESIZABLE);
 }
